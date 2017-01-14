3e022ea6f8236ca29f3784341a49fb7bba1dce94
Benjamin Strauß <benmastra@gmail.com>
2017-01-14 22:31:25 +0100

    simplifed code

--- Classes/Utils/LineBreakUtility.php
+++ Classes/Utils/LineBreakUtility.php
@@ -3,33 +3,30 @@
 namespace BStrauss\Engine\Utils;
 
 class LineBreakUtility {
-  static $hardBreakReplacement = '|';
-
-  static $softBreakReplacement = '|';
-
-  static $wordBreakReplacement = '|';
-
   /**
    * @param string $text
+   * @param string $match
    * @return string
    */
-  static function hardBreak($text) {
-    return str_replace(self::$hardBreakReplacement, '<br>', $text);
+  static function hardBreak($text, $match = '\|') {
+    return mb_ereg_replace($match, '<br>', $text);
   }
 
   /**
    * @param string $text
+   * @param string $match
    * @return string
    */
-  static function softBreak($text) {
-    return str_replace(self::$softBreakReplacement, '&shy;', $text);
+  static function softBreak($text, $match = '\|') {
+    return mb_ereg_replace($match, '&shy;', $text);
   }
 
   /**
    * @param string $text
+   * @param string $match
    * @return string
    */
-  static function wordBreak($text) {
-    return str_replace(self::$wordBreakReplacement, '<wbr>', $text);
+  static function wordBreak($text, $match = '\|') {
+    return mb_ereg_replace($match, '<wbr>', $text);
   }
 }
