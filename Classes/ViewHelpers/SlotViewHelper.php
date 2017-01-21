040add4dcab6abb22fea9e85e0766f0b48b42386
Benjamin Strauß <benmastra@gmail.com>
2017-01-21 14:32:45 +0100

    update for typo 8.4

--- Classes/ViewHelpers/SlotViewHelper.php
+++ Classes/ViewHelpers/SlotViewHelper.php
@@ -3,18 +3,19 @@
 namespace BStrauss\Engine\ViewHelpers;
 
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 class SlotViewHelper extends AbstractViewHelper {
+  use CompileWithRenderStatic;
+
   public function initializeArguments() {
     parent::initializeArguments();
 
     $this->registerArgument('name', 'string', 'Name of the slot. The result name inside the partial will be prefixed with "slot-".', true);
   }
 
-  /**
-   * @return string
-   */
-  public function render() {
-    return $this->renderChildren();
+  public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext) {
+    return $renderChildrenClosure();
   }
 }
