f03508a4278d64547da8bd6a2b1e52059285e648
Benjamin Strauß <b.strauss@netzbewegung.com>
2017-02-24 16:47:50 +0100

    update version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,9 +10,9 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0',
-            'typo3' => '8.4.0',
-            'vhs' => '3.1.0',
+            'php' => '7.0.0-7.1.99',
+            'typo3' => '8.5.0-8.5.99',
+            'vhs' => '4.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
